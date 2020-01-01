ca1e231f3ab3de8025ce7be21459923dcf3c3571
Mario <pyrros2000@gmail.com>
2020-01-01 11:35:54 -0800

    Close button now works by preventing function from handle click from bubbling up

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class App {
         this.$noteTitle = document.querySelector('#note-title');
         this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
+        this.$formCloseButton = document.querySelector('#form-close-button');
         this.addEventListners();
     }
 
@@ -15,6 +16,7 @@ class App {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
         });
+
         this.$form.addEventListener('submit', event => {
             event.preventDefault();
             const title = this.$noteTitle.value;
@@ -25,14 +27,24 @@ class App {
                 this.addNote({ title, text });
             }
         });
+        this.$formCloseButton.addEventListener('click', event => {
+            event.stopPropagation();
+            this.closeForm();
+        });
     }
 
     handleFormClick() {
         const isFormClicked = this.$form.contains(event.target);
 
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote = title || text;
+
         if(isFormClicked) {
             //open form
             this.openForm();
+        } else if(hasNote) {
+            this.addNote({ title, text });
         } else {
             //close form
             this.closeForm();
@@ -53,10 +65,10 @@ class App {
         this.$noteText.value = '';
     }
 
-    addNote(note) {
+    addNote({ title, text }) {
         const newNote = {
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: 'white',
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
