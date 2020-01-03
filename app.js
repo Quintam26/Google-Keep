4b06be26f555fd49c88ff3a79d91ccc75890d0c2
Mario <pyrros2000@gmail.com>
2020-01-02 21:19:26 -0800

    notes can now be deleted based on their data-id in the DOM

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class App {
             this.handleFormClick(event);
             this.selectNote(event);
             this.openModal(event);
+            this.deleteNote(event);
         });
 
         document.body.addEventListener('mouseover', event => {
@@ -105,6 +106,8 @@ class App {
     }
 
     openModal(event) {
+        if(event.target.matches('.toolbar-delete')) return;
+
         if (event.target.closest('.note')) {
            this.$modal.classList.toggle('open-modal');  
            this.$modalTitle.value = this.title;
@@ -117,7 +120,7 @@ class App {
          this.$modal.classList.toggle('open-modal');  
      }
 
-     openTooltip(event) {
+    openTooltip(event) {
         if (!event.target.matches('.toolbar-color')) return;
         this.id = event.target.dataset.id; 
         const noteCoords = event.target.getBoundingClientRect();
@@ -125,7 +128,7 @@ class App {
         const vertical = noteCoords.top + window.scrollY;
         this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
         this.$colorTooltip.style.display = 'flex'
-      }
+    }
     
     closeTooltip(event) {
         if (!event.target.matches('.toolbar-color')) return;
@@ -167,7 +170,15 @@ class App {
         this.title = $noteTitle.innerText;
         this.text = $noteText.innerText;
         this.id = $selectedNote.dataset.id;
-     }
+    }
+
+    deleteNote(event) {
+        event.stopPropagation();
+        if(!event.target.matches('.toolbar-delete')) return;
+        const id = event.target.dataset.id;
+        this.notes = this.notes.filter(note => note.id !== Number(id));
+        this.displayNotes();
+    }
 
     displayNotes() {
         const hasNotes = this.notes.length > 0;
@@ -184,7 +195,7 @@ class App {
             <div class="toolbar-container">
                 <div class="toolbar">
                     <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette">
-                    <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                    <img class="toolbar-delete" data-id=${note.id} src="https://icon.now.sh/delete">
                 </div>
             </div>
             </div>
